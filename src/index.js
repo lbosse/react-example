b0161363efd09536aed084157445bab0cddf9f81
Luke Bosse <lukebosse@u.boisestate.edu>
2018-03-13 08:51:42 -0600

    showing heidi class repo workflow

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ class Board extends React.Component {
 
     render() {
         return (
+            <span> Hello world!!! </span>
             <div>
             <div className="board-row">
             {this.renderSquare(0)}
